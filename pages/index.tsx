40b88fdd1dbd598db8e1d1f158980962b3134500
samy senani <aceportgasd235@gmail.com>
2022-09-13 10:26:40 +0100

    add: Error handing custom error page 404, 500 pages
    add: custom error page --> _error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import LanguageSwitcher from "../components/LanguageSwitcher";
+
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <LanguageSwitcher />
+    </div>
+  );
 };
 
 export default Home;
