050d82ee036adfb12f229b16683c247ff85d5740
samy senani <aceportgasd235@gmail.com>
2022-09-14 15:16:19 +0100

    add: useRef hook

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 
 const Blog = () => {
   const [count, setCount] = useState(0);
@@ -19,12 +19,36 @@ const Blog = () => {
     if (count === 3) setCount(0);
   }, [count]);
 
+  // ref
+  const countRender = useRef<any>();
+  const [inputValue, setInputValue] = useState("");
+
+  const focusInput = () => {
+    countRender.current.focus();
+  };
+
+  useEffect(() => {
+    countRender.current = countRender.current + 1;
+  });
+
+  console.log(countRender);
+
   return (
     <div>
       <h1>useEffect</h1>
       <button onClick={() => setCount(count + 1)}> + </button>
       <span> {count} </span>
       <button onClick={() => setCount(count - 1)}> - </button>
+
+      <h1>useRef</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <span>Render Count: {countRender.current}</span>
+
+      <h1>useMemo</h1>
     </div>
   );
 };
