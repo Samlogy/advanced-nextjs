ca001016f550a1bc48f1af08a9490207e1a786dc
samy senani <aceportgasd235@gmail.com>
2022-09-14 15:02:57 +0100

    add: useEffect / useState example

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from "next";
+import { useState, useEffect } from "react";
 
-const Blog: NextPage = () => {
-  return <div>Blog</div>;
+const Blog = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // on mount
+    let timer = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    // unmount
+    return () => clearTimeout(timer);
+  }, []);
+
+  // updated
+  useEffect(() => {
+    if (count === 3) setCount(0);
+  }, [count]);
+
+  return (
+    <div>
+      <h1>useEffect</h1>
+      <button onClick={() => setCount(count + 1)}> + </button>
+      <span> {count} </span>
+      <button onClick={() => setCount(count - 1)}> - </button>
+    </div>
+  );
 };
 
 export default Blog;
