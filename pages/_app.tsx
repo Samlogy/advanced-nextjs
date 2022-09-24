5600dfebe41a3913288335c4731e0044086c2050
samy senani <aceportgasd235@gmail.com>
2022-09-24 09:09:43 +0100

    add: custom theme -colors -fonts -define default color scheme -define default variant
    -components (button-checkbox-select-input)
    --> redefine some propreties: variants, color, _hover, _focus, _active, style...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import TodosProvider from "../contexts/todoContext";
+import theme from "../chakra/theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <TodosProvider>
         <Component {...pageProps} />
       </TodosProvider>
