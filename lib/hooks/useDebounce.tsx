3ddfda2c079a929bbfc1c888e849a20b256545c7
samy senani <aceportgasd235@gmail.com>
2022-09-24 11:49:18 +0100

    fix: useDebounce hook

--- lib/hooks/useDebounce.tsx
+++ lib/hooks/useDebounce.tsx
@@ -1,19 +1,7 @@
 import { useCallback, useRef } from "react";
+import { debounce } from "../functions";
 
-export default function useDebounce(fn: any, delay = 500) {
-  function debounce(func: any) {
-    let timer: any;
-    return (...args: any) => {
-      const context = this;
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        timer = null;
-        func.apply(context, args);
-      }, 650);
-    };
-  }
-
-  let debounced = useCallback(debounce(fn), [delay]);
-  debounced = useRef(debounced).current;
-  return debounced;
+export default function useDebounce(fn: any, delay?: number) {
+  let debounced = useCallback(debounce(fn, delay), []);
+  return useRef(debounced).current;
 }
