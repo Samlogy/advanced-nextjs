0453d3fa496662f9373699c2a4215b6c5a9cdcd1
samy senani <aceportgasd235@gmail.com>
2022-10-10 11:00:42 +0100

    add: basic animation (on load) add: basic props (control)

---
+++ pages/challenges.tsx
@@ -0,0 +1,115 @@
+import { Heading } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import {
+  MdOutlineArrowBackIosNew,
+  MdOutlineArrowForwardIos,
+} from 'react-icons/md';
+
+import screen from '../public/images/screen.png';
+import skate from '../public/images/skateboard.jpg';
+
+export default function Challenges() {
+  const images = [skate, screen];
+  return (
+    <div>
+      <Heading as="h2" textAlign="center" mb="1em">
+        Modal
+      </Heading>
+      <Carousel data={images} isAutoPlay speed={1500} />
+    </div>
+  );
+}
+
+interface ICarousel {
+  data: any;
+  isAutoPlay?: boolean;
+  speed?: number;
+}
+
+function Carousel({ data, isAutoPlay = false, speed = 500 }: ICarousel) {
+  const [currentSlide, setSlide] = useState(1);
+
+  const NB_MAX = 2; //useMemo(() => data.length, []);
+
+  function onPrev() {
+    if (currentSlide === 1) {
+      setSlide(NB_MAX);
+      return;
+    }
+    setSlide((prev) => prev - 1);
+  }
+  function onNext() {
+    if (currentSlide === NB_MAX) {
+      setSlide(1);
+      return;
+    }
+    setSlide((prev) => prev + 1);
+  }
+  function goTo(index: number) {
+    console.log(index + 1);
+    setSlide(index + 1);
+  }
+
+  function onAutoPlay() {
+    setInterval(onNext, speed);
+  }
+  // auto play (autoPlay, speed)
+  useEffect(() => {
+    isAutoPlay && onAutoPlay();
+    return () => {
+      onAutoPlay();
+    };
+  }, []);
+
+  return (
+    <div className="slider-container">
+      <div className="arrow left" onClick={onPrev}>
+        <MdOutlineArrowBackIosNew size={24} />
+      </div>
+      <div className="arrow right" onClick={onNext}>
+        <MdOutlineArrowForwardIos size={24} />
+      </div>
+
+      <AnimatePresence>
+        <div className="slider">
+          {data.map((el: any, idx: number) => {
+            const isVisible = currentSlide === idx + 1 ? true : false;
+            return (
+              <motion.img
+                key={idx}
+                src={el.src}
+                alt=""
+                loading="lazy"
+                className={isVisible ? 'slider-image active' : 'slider-image'}
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.5,
+                  type: 'tween',
+                }}
+              />
+            );
+          })}
+        </div>
+      </AnimatePresence>
+
+      <div className="slider-indicators">
+        {data.map((_, idx: number) => {
+          const isVisible = currentSlide === idx + 1 ? true : false;
+          return (
+            <span
+              key={idx}
+              className={isVisible ? 'active' : ''}
+              onClick={() => goTo(idx)}
+            ></span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
