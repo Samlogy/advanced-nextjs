dcfd59be4cbc3568c7aebbf25957fd5cbedbc2b7
samy senani <aceportgasd235@gmail.com>
2022-10-10 22:24:06 +0100

    add: alert cpt

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -1,143 +1,47 @@
 import { Heading } from '@chakra-ui/react';
-import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
-import ReactDOM from 'react-dom';
-import {
-  MdOutlineArrowBackIosNew,
-  MdOutlineArrowForwardIos,
-  MdOutlineClose,
-} from 'react-icons/md';
+import { MdOutlineClose } from 'react-icons/md';
 
 import screen from '../public/images/screen.png';
 import skate from '../public/images/skateboard.jpg';
 
+import Modal from '../components/Modal';
+
 export default function Challenges() {
   const images = [skate, screen];
   const [isOpen, setOpen] = useState(false);
+
+  // <Carousel data={images} isAutoPlay speed={1000} />
+  const msg =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus hic saepe deleniti eius obcaecati voluptates quidem, nulla iure, esse molestias, consequuntur voluptas atque. Aspernatur temporibus a ipsam velit dolores culpa.';
   return (
     <div>
       <Heading as="h2" textAlign="center" mb="1em">
         Modal
       </Heading>
-      {/*
-        <Carousel data={images} isAutoPlay speed={1000} />
-      */}
       <button onClick={() => setOpen(true)}> open </button>
       <Modal isOpen={isOpen} onClose={() => setOpen(false)} />
+
+      <Alert status="success" message={msg} isClose />
     </div>
   );
 }
 
-interface IModal {
-  isOpen: boolean;
-  onClose: any;
-  header?: any;
-  body?: any;
-  footer?: any;
-}
-function Modal({ isOpen, onClose, header, body, footer }: IModal) {
-  if (isOpen)
-    return ReactDOM.createPortal(
-      <div className="modal--overlay" onClick={onClose}>
-        <div className="modal-container" onClick={(e) => e.stopPropagation()}>
-          <span className="modal--close" onClick={onClose}>
-            <MdOutlineClose color="black" size={24} />
-          </span>
-          <div className="modal--content">
-            {header && <div className="modal--header">{header}</div>}
-            {body && <div className="modal--body">{body}</div>}
-            {footer && <div className="modal--footer"> {footer} </div>}
-          </div>
-        </div>
-      </div>,
-      document.body
-    );
-  return null;
-}
-
-interface ICarousel {
-  data: any;
-  isAutoPlay?: boolean;
-  speed?: number;
-}
-
-function Carousel({ data, isAutoPlay = false, speed = 500 }: ICarousel) {
-  const [currentSlide, setSlide] = useState(1);
-
-  const NB_MAX = 2; //useMemo(() => data.length, []);
-
-  function onPrev() {
-    if (currentSlide === 1) {
-      setSlide(NB_MAX);
-      return;
-    }
-    setSlide((prev) => prev - 1);
-  }
-  function onNext() {
-    if (currentSlide === NB_MAX) {
-      setSlide(1);
-      return;
-    }
-    setSlide((prev) => prev + 1);
-  }
-  function goTo(index: number) {
-    setSlide(index + 1);
-  }
-
-  function onAutoPlay() {
-    setInterval(onNext, speed);
-  }
-
-  //isAutoPlay && onAutoPlay();
-
+function Alert({ status, message, isClose }: any) {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <div className="slider-container">
-      <div className="arrow left" onClick={onPrev}>
-        <MdOutlineArrowBackIosNew size={24} />
+    <div className={`alert--container ${status}`}>
+      <div>
+        <MdOutlineClose size={22} onClick={() => setOpen(false)} />
       </div>
-      <div className="arrow right" onClick={onNext}>
-        <MdOutlineArrowForwardIos size={24} />
-      </div>
+      <p className="alert--message"> {message} </p>
 
-      <AnimatePresence>
-        <div className="slider">
-          {data.map((el: any, idx: number) => {
-            const isVisible = currentSlide === idx + 1 ? true : false;
-            return (
-              <motion.img
-                key={idx}
-                src={el.src}
-                alt=""
-                loading="lazy"
-                className={isVisible ? 'slider-image active' : 'slider-image'}
-                initial={{
-                  opacity: 0,
-                }}
-                animate={{
-                  opacity: 1,
-                }}
-                transition={{
-                  duration: 0.5,
-                  type: 'tween',
-                }}
-              />
-            );
-          })}
-        </div>
-      </AnimatePresence>
-
-      <div className="slider-indicators">
-        {data.map((_, idx: number) => {
-          const isVisible = currentSlide === idx + 1 ? true : false;
-          return (
-            <span
-              key={idx}
-              className={isVisible ? 'active' : ''}
-              onClick={() => goTo(idx)}
-            ></span>
-          );
-        })}
-      </div>
+      {isClose && (
+        <MdOutlineClose
+          className="alert--close"
+          onClick={() => setOpen(false)}
+        />
+      )}
     </div>
   );
 }
