4de62ddef8d3ef65541f8a1bfacefca18f791ae9
samy senani <aceportgasd235@gmail.com>
2022-11-05 09:53:17 +0100

    add: example on memo / callback (main differences)

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
-import { useState } from 'react';
-import SideBar from '../components/SideBar';
+import { useCallback, useMemo, useState } from 'react';
 
 import screen from '../public/images/screen.png';
 import skate from '../public/images/skateboard.jpg';
 
+import { motion } from 'framer-motion';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import LanguageSwitcher from '../components/LanguageSwitcher';
 
 export default function Challenges() {
@@ -24,12 +25,13 @@ export default function Challenges() {
       <Modal isOpen={isOpen} onClose={() => setOpen(false)} />
   */
   // <SideBar />
+  // <NavBar />
+
   const msg: string =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus hic saepe deleniti eius obcaecati voluptates quidem, nulla iure, esse molestias, consequuntur voluptas atque. Aspernatur temporibus a ipsam velit dolores culpa.';
   return (
     <div>
-      <NavBar />
-      <SideBar />
+      <MemoVsCallback />
     </div>
   );
 }
@@ -59,12 +61,29 @@ const LINKS = [
 function NavBar() {
   const [isOpen, setOpen] = useState(false);
   return (
-    <div className="navbar--container">
+    <motion.div
+      initial={{ y: -25, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        duration: 0.75,
+      }}
+      className="navbar--container"
+    >
+      <div className="navbar--toggle">
+        <button
+          type="button"
+          className="btn-icon"
+          onClick={() => setOpen(!isOpen)}
+        >
+          {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+        </button>
+      </div>
+
       <div className="navbar--content">
         <div className="navbar--logo">Logo</div>
-        <>
+        <div className="navbar--tools">
           <LanguageSwitcher />
-        </>
+        </div>
         <div className="navbar--links">
           {LINKS.map((el: any, idx: number) => (
             <div key={idx} className="navbar--item">
@@ -75,6 +94,65 @@ function NavBar() {
           ))}
         </div>
       </div>
+    </motion.div>
+  );
+}
+
+// useMemo vs useCallback
+function MemoVsCallback() {
+  const [counter1, setCounter1] = useState(0); // Counter 1
+  const [counter2, setCounter2] = useState(0); // Counter 1
+  // const expensiveValue = computeExpensiveValue(counter1); --> computed on every component render
+  const expensiveValueMemoized = useMemo(
+    () => computeExpensiveValue(counter1),
+    [counter1]
+  );
+
+  const memoizedCallback = useCallback(
+    () => computeExpensiveValue(counter1),
+    [counter1]
+  );
+  const memoizedValue = useMemo(
+    () => computeExpensiveValue(counter1),
+    [counter1]
+  );
+
+  const memoizedCallbackCalled = useCallback(computeExpensiveValue, [counter1]);
+
+  console.log('memoized function: ', memoizedCallback); // return uncalled function (so i can call it later)
+  console.log('memoized value: ', memoizedValue); // calls the function & returns it value
+  console.log('memoized function called: ', memoizedCallbackCalled(counter1)); // calls the function & returns it value (similar to useMemo)
+
+  // callback: returns a function which is referentially equal between renders
+  // memo: calculte & returns value which referentially equal between renders
+  // referential equal: values are equal a do not trigger a re-render --> great form optimization
+
+  return (
+    <div>
+      <div>
+        <h2>Counter 1: {counter1}</h2>
+        <button onClick={() => setCounter1(counter1 + 1)}>+</button>
+      </div>
+      <div>
+        <h2>Counter 2: {counter2}</h2>
+        <button onClick={() => setCounter2(counter2 + 1)}>+</button>
+      </div>
+
+      <div>
+        <h2>Expensive Value Memoized:</h2>
+        {expensiveValueMemoized}
+      </div>
     </div>
   );
 }
+
+// Expensive Function
+const computeExpensiveValue = (count: number) => {
+  // Display on console whenever the function gets call
+  console.log('This function is running...');
+  // Time Consuming Calcuation
+  for (let i = 0; i < 1000000000; i++) {
+    count += 1;
+  }
+  return count;
+};
